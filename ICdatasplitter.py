e602036e6938915ad6e010784fe88a16a0e9123c
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-06-24 16:12:55 -0400

    sample data

--- ICdatasplitter.py
+++ ICdatasplitter.py
@@ -13,7 +13,7 @@ data = data_split[0]
 data = data.drop('Unnamed: 0',axis=1)
 
 #for each row, print the stuff from "text" into a txt file
-for row in range(len(data)):
+for row in range(20200, 20210):
     fnm = data['class'][row]+str(row)+".txt"
     this = open(fnm,"w", encoding="utf-8")
     this.write(data['text'][row])
