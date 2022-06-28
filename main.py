54dab139fb474b9716c44adcb64b8378a3b406a9
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-06-28 18:46:48 -0400

    works locally

--- main.py
+++ main.py
@@ -27,40 +27,42 @@ def Classify(text):
         st.error("This text discusses suicide or displays possible suicidal ideation.")
 
 def main():
-    #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\Users\maria\OneDrive\Documents\School Summer 2022\IdeationClassification\ideationclassification-d09633ba31d7.json"
+    #works locally
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\Users\maria\OneDrive\Documents\School Summer 2022\IdeationClassification\ideationclassification-d09633ba31d7.json"
+
+    #need to get working remotely
+    # cred = st.secrets["gc_serviceaccount"]
+    # cred = json.dumps(cred)
+    #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=cred
+
+    # with tempfile.NamedTemporaryFile(mode='w') as fp:
+    #     json.dump(cred, fp, indent=2, separators=(", ", ": "))
+    #     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = fp.name    
+
+    st.title("Classifying Reddit Posts")
+    st.write("This form interacts with an AI model which was trained to identify text as pertaining to suicide or demonstrating suicidal ideation.")
+    #st.write ("This model was trained on Reddit posts, but should be able to function with any text.")
+    #st.write("Please enter text in the field below, or select one of the population options.")
+
+    box = st.radio('', ('Enter Text', 'Populate Dream Reddit Post', 'Populate Suicide Reddit Post'))
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
     
-    cred = st.secrets["gc_serviceaccount"]
-    cred = json.dumps(cred)
+    if box == "Populate Dream Reddit Post":
+        fill_text = getPost('Dreams')
+        st.text_area('', fill_text)
+        text = fill_text
+    elif box == "Populate Suicide Reddit Post":
+        fill_text = getPost('SuicideWatch')
+        st.text_area('', fill_text)
+        text = fill_text
+    else:
+        text = st.text_area('')
 
-    with tempfile.NamedTemporaryFile(mode='w') as fp:
-        json.dump(cred, fp, indent=2, separators=(", ", ": "))
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = fp.name
-        
-        #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=cred
-        st.title("Classifying Reddit Posts")
-        st.write("This form interacts with an AI model which was trained to identify text as pertaining to suicide or demonstrating suicidal ideation.")
-        #st.write ("This model was trained on Reddit posts, but should be able to function with any text.")
-        #st.write("Please enter text in the field below, or select one of the population options.")
-
-        box = st.radio('', ('Enter Text', 'Populate Dream Reddit Post', 'Populate Suicide Reddit Post'))
-        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-        
-        if box == "Populate Dream Reddit Post":
-            fill_text = getPost('Dreams')
-            st.text_area('', fill_text)
-            text = fill_text
-        elif box == "Populate Suicide Reddit Post":
-            fill_text = getPost('SuicideWatch')
-            st.text_area('', fill_text)
-            text = fill_text
+    if st.button("Classify"):
+        if text != '':
+            Classify(text)
         else:
-            text = st.text_area('')
-
-        if st.button("Classify"):
-            if text != '':
-                Classify(text)
-            else:
-                st.error("Please provide text to classify.")
+            st.error("Please provide text to classify.")
 
 if __name__ == "__main__":
        main() 
